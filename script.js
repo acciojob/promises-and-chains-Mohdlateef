d1a63c948fc55d0320878043be3390a9a7c3fd09
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-01-27 23:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If requi
+document.getElementById("btn").addEventListener("click",resolve);
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+// alert(name.value);
+function resolve(e) {
+	e.preventDefault();
+	let b=name.value;
+	alert(b)
+	let a=age.value;
+	let promise=new Promise((rest,resol)=>{
+		if(a>18)
+		{
+			
+		}
+	})
+	
+}
